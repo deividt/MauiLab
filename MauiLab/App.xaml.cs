d48f8a35f960b61e18fc08d53699b700dc959724
Deividt gemeli <deividt@gmail.com>
2022-07-22 00:15:22 -0300

    Created MAUI Lab Project

---
+++ MauiLab/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiLab;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MauiLab/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiLab;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
