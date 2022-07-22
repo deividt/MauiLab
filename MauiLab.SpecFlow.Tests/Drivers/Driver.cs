8d75b7b19f18edcb0891cc9c575ba85ddad0de8e
Deividt gemeli <deividt@gmail.com>
2022-07-22 11:07:57 -0300

    Added specflow project and basic test

---
+++ MauiLab.SpecFlow.Tests/Drivers/Driver.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace MauiLab.SpecFlow.Tests.Drivers
+{
+    public class Driver
+    {
+    }
+}
---
+++ MauiLab.SpecFlow.Tests/Hooks/Hook.cs
@@ -0,0 +1,10 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace MauiLab.SpecFlow.Tests.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+    }
+}
